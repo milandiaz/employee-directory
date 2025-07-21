bbbd549ae40118ffb6f51587678936c2d3b357a9
milandiaz <milandiaz2000@hotmail.com>
2025-07-21 11:49:38 -0500

    Passed all tests

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 import express from "express";
-import employees from "#employees.js";
+import employees from "./db/employees.js";
+
 const app = express();
 
 export default app;
 
 app.route("/").get((req, res) => {
-  res.send("You've reached the Playlist API!");
+  res.send("Hello employees!");
 });
 app.route("/employees").get((req, res) => {
   res.send(employees);
 });
-app.route("/employees/:index").get((req, res) => {
-  const { index } = req.params;
-  const employee = employees[index];
+app.route("/employees/random").get((req, res) => {
+  const random = Math.floor(Math.random() * employees.length);
+  res.send(employees[random]);
+});
+app.route("/employees/:id").get((req, res) => {
+  const { id } = req.params;
+  const employee = employees.find((emp) => emp.id === parseInt(id));
 
   // We have to `return` in this guard case to avoid sending multiple responses!
   if (!employee)
     return res.status(404).send("That employee does not exist in the list.");
 
-  res.send(song);
+  res.send(employee);
 });
